49e256fbb3d136aa03fa46994260fa3beee3568f
Hillarywriter <121403806+Hillarywriter@users.noreply.github.com>
2022-12-26 17:47:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//Cart
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector("#.cart");
+let closeCart = document.querySelector("#close-cart");
+    //Open cart
+cartIcon.onclick = () => {
+    cart.classList.add("active");
+};
+    //Close cart
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+};
+
+//Cart Working
+if (document.readyState == "loading"){
+    document.addEventListener("DOMContentLoaded", ready);
+} else{
+    ready();
+}
+//Making function
+function ready(){
+    //remove items from cart
+    var removeCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(removeCartButtons);
+    for (var i = 0, i < removeCartButtons.length; i++){
+        var button =removeCartButtons [i];
+        button.addEventListener("click", removeCartItem);
+    }
+}
+//remove Items from Cart
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+}
